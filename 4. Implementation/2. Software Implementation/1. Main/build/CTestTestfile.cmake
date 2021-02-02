8c81655ad9cce10ee7a92bf443abdd8e167283dc
h0wl-14 <josepr.mendes@gmail.com>
2021-02-02 15:05:38 +0000

    ADD|UPTD: Reworked project structure and CMake/VSCode debugging enabled

---
+++ 4. Implementation/2. Software Implementation/1. Main/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/josep/Desktop/ARGOS/argos/4. Implementation/2. Software Implementation/1. Main
+# Build directory: /home/josep/Desktop/ARGOS/argos/4. Implementation/2. Software Implementation/1. Main/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
