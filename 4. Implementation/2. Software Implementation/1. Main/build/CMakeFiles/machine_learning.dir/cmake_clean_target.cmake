c744ced978b8e7324026674614d811b5610f2c3f
h0wl-14 <josepr.mendes@gmail.com>
2021-02-04 03:15:46 +0000

    ADMIN: Solving conflicts

--- 4. Implementation/2. Software Implementation/1. Main/build/CMakeFiles/machine_learning.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libmachine_learning.a"
-)
--- 4. Implementation/2. Software Implementation/1. Main/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/silent/Desktop/ARGOS/argos/4. Implementation/2. Software Implementation/1. Main
-# Build directory: /home/silent/Desktop/ARGOS/argos/4. Implementation/2. Software Implementation/1. Main/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
