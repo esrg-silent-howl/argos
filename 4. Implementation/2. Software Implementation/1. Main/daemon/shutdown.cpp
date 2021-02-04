439b066f136eb56e82cf5d44c0a5a79af3a6d5eb
h0wl-14 <josepr.mendes@gmail.com>
2021-02-04 03:06:09 +0000

    Merging

--- 4. Implementation/2. Software Implementation/1. Main/daemon/shutdown.cpp
+++ 4. Implementation/2. Software Implementation/1. Main/daemon/shutdown.cpp
@@ -1,8 +1,86 @@
-#include <iostream>
+#include "shutdown.hpp"
 
-int main (int argc, char** argv) {
+void daemonSetup(){
 
-	std::cout << "Hello, world!" << std::endl;
+    pid_t pid;
 
-	return 0;
+    /* Fork off the parent process and create a new child process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+		//syslog(LOG_ERR, "%s\n", "Fork");
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent process terminate */
+    if (pid > 0){
+        exit(EXIT_SUCCESS);
+	}
+
+	/*Start a new session ( setsid() )*/
+    /* On success: The child process becomes session leader now detached 
+	from its controlling terminal (CTTY)*/
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+    //signal(SIGCHLD, SIG_IGN);
+    //signal(SIGHUP, SIG_IGN);
+
+	/* Redundancy Fork*/
+    /* Fork off for the second time to ensure that the daemon doesn't
+	have access to the TTY again*/
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0){
+		//syslog(LOG_ERR, "%s\n", " Second Fork");
+        exit(EXIT_FAILURE);
+	}
+
+    /* Success: Let the parent terminate */
+    if (pid > 0){
+		//printf("Deamon PID: %d\n", pid);
+        exit(EXIT_SUCCESS);
+	}
+
+    /* Set new file permissions by changing the file mode mask:
+	may vary depending on the daemon's permissions */
+    umask(0);
+
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/");
+
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    {
+        close (x);
+    }
+
+    /* Open the log file */
+    openlog ("shutdownDaemon", LOG_PID, LOG_DAEMON);
 }
+
+int main()
+{
+    daemonSetup();
+
+    while (1)
+    {
+        //TODO: Insert daemon code here.
+        syslog (LOG_NOTICE, "Shutdown Daemon Up and Running.");
+        sleep (20);
+        //break;
+    }
+
+    syslog (LOG_NOTICE, "Shutdown Daemon Terminated.");
+    closelog();
+
+    return EXIT_SUCCESS;
+}
+
+/* ps -xj | grep shutdownDaemon 
+   grep shutdownDaemon /var/log/syslog */
---
+++ 4. Implementation/2. Software Implementation/1. Main/daemon/shutdown.hpp
@@ -0,0 +1,17 @@
+#ifndef _SHUTDOWN_HPP
+#define _SHUTDOWN_HPP
+
+#include  <sys/syslog.h>
+#include <sys/types.h> 
+#include <sys/stat.h>
+#include <unistd.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h> 
+#include <fcntl.h>
+#include <time.h>
+#include <signal.h>
+
+extern void daemonSetup();
+
+#endif
